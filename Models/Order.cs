2cba65868b048440c32434f53400927d09f3bc5e
santo1827 <136288759+santo1827@users.noreply.github.com>
2025-05-26 17:19:16 +1000

    Referencing error

--- Models/Order.cs
+++ Models/Order.cs
@@ -17,14 +17,27 @@ namespace ArribaEats.Models
         public string RestaurantName { get; set; }
         public string CustomerEmail { get; set; }
         public string DelivererEmail { get; set; } = null;
+        public string RestaurantLocation { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLocation { get; set; }
+
         public OrderStatus Status { get; set; } = OrderStatus.Ordered;
         public Dictionary<string, int> Items { get; set; } = new();
 
-        public Order(int orderNumber, string restaurantName, string customerEmail)
+        public Order(
+            int orderNumber,
+            string restaurantName,
+            string restaurantLocation,
+            string customerEmail,
+            string customerName,
+            string customerLocation)
         {
             OrderNumber = orderNumber;
             RestaurantName = restaurantName;
+            RestaurantLocation = restaurantLocation;
             CustomerEmail = customerEmail;
+            CustomerName = customerName;
+            CustomerLocation = customerLocation;
         }
     }
 }
